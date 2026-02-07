a730c291e62ad34f8ce34b5f001a2591273fa1f5
Payer-Abbas <167884581+Payer-Abbas@users.noreply.github.com>
2026-02-07 12:20:46 +0530

    feat(localisation) -optimized localisation caching

--- apps/builder/src/features/settings/components/LocalizationForm.tsx
+++ apps/builder/src/features/settings/components/LocalizationForm.tsx
@@ -5,7 +5,6 @@ import { Field } from "@typebot.io/ui/components/Field";
 import { Switch } from "@typebot.io/ui/components/Switch";
 import { PlusSignIcon } from "@typebot.io/ui/icons/PlusSignIcon";
 import { TrashIcon } from "@typebot.io/ui/icons/TrashIcon";
-import { Button } from "@typebot.io/ui/components/Button";
 import { BasicAutocompleteInput } from "@/components/inputs/BasicAutocompleteInput";
 import { commonLanguages } from "@typebot.io/lib/languages";
 import { useState } from "react";
